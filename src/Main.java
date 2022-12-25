40c1ce79fb2707efc64d2d1013354648e93163f9
shigaboba <shigapovalrm@gmail.com>
2022-12-25 16:35:32 +0300

    Сделала задачи 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Собака " + dog + ",кошка " + cat + ", бумага " + paper);
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println("Собака " + dog + ",кошка " + cat + ", бумага " + paper);
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println("Собака " + dog + ",кошка " + cat + ", бумага " + paper);
     }
 }
